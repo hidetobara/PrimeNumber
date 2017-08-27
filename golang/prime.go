55ad040aa1fb5672291cc474a085712c909132ef
hidetobara <hidetobara@gmail.com>
2017-08-27 17:35:00 +0900

    add golang

---
+++ golang/prime.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	const limit = 10000
+	start := time.Now()
+	primes := []int{}
+	for n := 2; ; n++ {
+		if !isPrime(n) {
+			continue
+		}
+		primes = append(primes, n)
+		//fmt.Printf("%d\n", n)
+		if len(primes) == limit {
+			fmt.Printf("%dth prime is %d\n", limit, n)
+			break
+		}
+	}
+	duration := time.Now().Sub(start)
+	fmt.Printf("duration=%d msec", duration.Nanoseconds() / (1000*1000))
+}
+
+func isPrime(number int) bool {
+	for n := 2; n < number / 2; n++ {
+		if number % n == 0 {
+			return false
+		}
+	}
+	return true
+}
