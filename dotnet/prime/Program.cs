01253ed0b02a3d3a221b6831754a821f14720686
Hideto Obara <hidetobara@gmail.com>
2017-10-04 23:24:23 +0900

    add dotnet

---
+++ dotnet/prime/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Stopwatch watch = new Stopwatch();
+			watch.Start();
+			List<int> list = new List<int>();
+			int number = 1;
+			while(true)
+			{
+				number++;
+				if (!IsPrime(number)) continue;
+				list.Add(number);
+				if(list.Count == 10000)
+				{
+					Console.WriteLine("10000th prime is " + number);
+					break;
+				}
+			}
+			watch.Stop();
+			Console.WriteLine("duration is " + (watch.ElapsedMilliseconds / 1000f).ToString("F1") + " sec");
+        }
+
+		static bool IsPrime(int number)
+		{
+			for (int n = 2; n < number / 2; n++)
+				if (number % n == 0) return false;
+			return true;
+		}
+    }
+}
