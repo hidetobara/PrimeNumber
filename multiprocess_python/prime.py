c662aede657d87e88beadc59a6fab4d6476410b4
obara <hidetobara@gmail.com>
2018-11-15 12:38:23 +0900

    modify multi process for python

--- multiprocess_python/prime.py
+++ multiprocess_python/prime.py
@@ -1,35 +1,47 @@
 import time
+import sys
 from multiprocessing import Process, Array
 
 def run():
+	STEP = 10000
+
+	limit = 10000
+	if len(sys.argv) == 2: limit = int(sys.argv[1])
+
 	start = time.time()
-	max = 120000
-	primes = Array('i', range(max))
+	count = 0
+	number = 2	
+	primes = Array('i', range(STEP))
+	while True:
+		p0 = Process(target=worker, args=(primes,number+0,number+STEP,number))
+		p0.start()
+		p1 = Process(target=worker, args=(primes,number+1,number+STEP,number))
+		p1.start()
+		p2 = Process(target=worker, args=(primes,number+2,number+STEP,number))
+		p2.start()
+		p3 = Process(target=worker, args=(primes,number+3,number+STEP,number))
+		p3.start()
+		p0.join()
+		p1.join()
+		p2.join()
+		p3.join()
 
-	p0 = Process(target=worker, args=(primes,2,max))
-	p0.start()
-	p1 = Process(target=worker, args=(primes,3,max))
-	p1.start()
-	p2 = Process(target=worker, args=(primes,4,max))
-	p2.start()
-	p3 = Process(target=worker, args=(primes,5,max))
-	p3.start()
-	p0.join()
-	p1.join()
-	p2.join()
-	p3.join()
-
-	number = 0;
-	for n in range(2, max, 1):
-		if primes[n]: number += 1
-		if number == 10000:
-			print("10000th prime is " + str(n))
-			break
+		for s in range(0, STEP, 1):
+			if not primes[s]:
+				continue
+			count += 1
+			if count == limit:
+				print(str(limit) + "th prime is " + str(number+s))
+				break
+		else:
+			number += STEP
+			continue
+		break
 	print("[mp.python] duration is " + str(time.time() - start) + " sec")
 
-def worker(box, start, end):
+def worker(box, start, end, offset):
 	for n in range(start, end, 4):
-		box[n] = is_prime(n)
+		box[ n - offset ] = is_prime(n)
 
 def is_prime(number):
 	for n in range(2, int(number/2)):
